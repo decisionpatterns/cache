75619bb0419f8157c67a2bdfa590e0dc8f502d08
Christopher Brown <cbrown@decisionpatterns.com>
2017-08-01 19:32:02 -0700

    Add dep to file.tools; fix tests

--- tests/testthat/test-cache.r
+++ tests/testthat/test-cache.r
@@ -1,49 +1,49 @@
-library(testthat) 
+library(testthat)
 library(magrittr)
+library(file.tools)
 
+test_that( "cache", {
 
-test_that( "cache", {   
-  
 context('cache')
-  
+
   expect_null( getOption('cache') ) # %>% expect_null
-  
+
   test   <- "test"
-  tmpdir <- tempdir() 
+  tmpdir <- tempdir()
 
 
 context( ".. cache")
 
   cache( test, cache = tmpdir )
-  
+
   rm( test )
-    expect_true( "test.rds" %in% dir(tmpdir) ) 
-    
-    
+    expect_true( "test.rds" %in% dir(tmpdir) )
+
+
 context( ".. uncache")
 
   # Unquoted
   uncache( test, tmpdir )
-    expect_true( exists("test") ) 
+    expect_true( exists("test") )
     expect_equivalent(test, "test")
     rm(test)  # clean-up
-  
+
   # Quoted
   uncache( "test", tmpdir )
-    expect_true( exists("test") ) 
+    expect_true( exists("test") )
     expect_equivalent(test, "test")
     rm(test)  # clean-up
-    
-  options( cache = tmpdir )    
+
+  options( cache = tmpdir )
   uncache( test )
-    expect_true( exists("test") ) 
+    expect_true( exists("test") )
     expect_equivalent(test, "test")
     rm(test)
-    
-    
+
+
 context( ".. uncache_all" )
 
-  # Clean-up 
+  # Clean-up
     options( cache = NULL )
     unlink( tmpdir, recursive = TRUE, force = TRUE )
 })
