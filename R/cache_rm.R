c07c8670532863c1e72273b1173e965d44114ced
Christopher Brown <cbrown@decisionpatterns.com>
2018-12-20 14:48:15 -0800

    Get cache_rm working

--- R/cache_rm.R
+++ R/cache_rm.R
@@ -2,9 +2,33 @@
 #'
 #' Remove item(s) from the cache
 #'
-#' @param ... character; names of cached objects to permanently remove from
-#' the cache
+#' @param ... single character vector of object names or comma separated list of
+#'            unquoted names.
 #' @param .cache path; cache directory
+#'
+#' @details
+#'
+#' [cache_rm()] accepts either a character vector -or- a comma-separated list of
+#' unquoted names.
+#'
+#' @examples
+#'
+#' \dontrun{
+#'   cache(iris)
+#'   cache_rm(iris)
+#'   cache(iris)
+#'   cache_rm('iris')
+#'
+#'   cache(iris);cache(mtcars)
+#'   cache_ls()
+#'   cache_rm(iris,mtcars)
+#'   cache_ls()
+#'
+#'
+#'   cache(iris);cache(mtcars)
+#'   cache_rm( c('iris','mtcars') )
+#'   cache_ls()
+#' }
 #'
 #' @md
 #' @export
@@ -13,11 +37,21 @@ cache_rm <- function( ..., .cache=cache_find() ) {
 
   items <- as.character( substitute( list(...) ))[-1]
 
-  fs::path( .cache, items )
+  li <- list(...)
+  if( length(li)==1 && is.character(li[[1]]) )
+    items <- li[[1]]
+
+  for( item in items ) {
+    # Find cached item by name
+     path <- name_to_path(item)
+     fs::file_delete(path)
+  }
 
 }
 
-
+#' @details
+#' [cache_delete()] is an alias for [cache_rm()].
+#'
 #' @rdname cache_rm
 #' @export
 
